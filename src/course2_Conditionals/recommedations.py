c79bc6c8afc5e598d58ab5b2ea6b6b1a48986f92
Vincent Yang <yangfans@msu.edu>
2025-12-09 18:01:41 -0500

    update

--- src/course2_Conditionals/recommedations.py
+++ src/course2_Conditionals/recommedations.py
@@ -1,6 +1,29 @@
 print("Answer follow questions and I can recommand games for you^^")
 def main():
-    difficulty = input("Difficult or Casual? ")
+
+    difficulty = input("Difficult or Casual?")
+    if not (difficulty == "Difficult" or difficulty == "Casual"):
+        print("Please enter a valid difficulty^^")
+        return
+    
+    players = input("Single or Multiple?")
+    if not (players == "Single" or players == "multiple"):
+        print("please enter a valid number of players(^*^)")
+        return
+    
+    if difficulty == "Difficult" and players == "Single":
+        recommand("Clash Royale")
+    
+    elif difficulty == "Difficult" and players == "multiple":
+        recommand("Brawl Stars")
+
+    elif difficulty == "Casual" and players == "Single":
+        recommand("Minecraft")
+    
+    elif difficulty == "Casual" and players == "multiple":
+        recommand("Poker")
+
+    '''difficulty = input("Difficult or Casual? ")
     players = input("Single or Multiple? ")
 
     if difficulty == "Difficult":
@@ -21,6 +44,7 @@ def main():
    
     else:
         print("Please choose one difficulty from top^.^")
+'''
     
 
 
