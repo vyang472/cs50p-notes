1d67a396a9be6541f6d7495700913bfefa57e206
Vincent Yang <yangfans@msu.edu>
2025-12-30 23:49:56 -0500

    update

--- src/course3Loops/cat.py
+++ src/course3Loops/cat.py
@@ -8,10 +8,28 @@ while i < 3:
 
 '''print("meow\n" * 3, end='')'''
 
-while True:
+'''while True:
     n = int(input("What is n? "))
     if n > 0:
         break
 
-print("meow\n"*n,end='')
+print("meow\n"*n,end='')'''
+
+def main():
+    number = get_number()
+    meow(number)
+
+def get_number():
+    while True:
+        n = int(input("what is n? "))
+        if n > 0:
+            return n
+def meow(n):
+    for _ in range(n):
+        print("meow")
+
+
+
+
+main()
 
