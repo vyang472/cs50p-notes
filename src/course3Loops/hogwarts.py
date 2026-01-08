77f1b17850f9b4a8534d62ce9a11d2c9e640ff5b
Vincent Yang <yangfans@msu.edu>
2026-01-07 23:58:52 -0500

    update

--- src/course3Loops/hogwarts.py
+++ src/course3Loops/hogwarts.py
@@ -5,11 +5,21 @@
     print(i + 1,students[i])
 '''
 
-students = {
+'''students = {
     "Hermione": "Gryffindor",
     "Harry": "Gryffindor",
     "Ron": "Gryffindor",
     "Draco": "Slytherin",
 }
 for student in students:
-    print(student, students[student], sep=",")
+    print(student, students[student], sep=",")'''
+
+students = [
+    {"name":'Hermione','house':'Gryffindor','patronus':'Otter'},
+    {"name":'Harry','house':'Gryffindor','patronus':'Stag'},
+    {"name":'Ron','house':'Gryffindor','patronus':'Jack Pussell terrier'},
+    {"name":'Draco','house':'Slytherin','patronus':None}
+]
+
+for student in students:
+    print(student['name'],student['house'],student['patronus'],sep=", ")
